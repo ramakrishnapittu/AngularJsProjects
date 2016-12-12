016371f03411dabc1fec294259f60031145c5721
Ramakrishna <ramainindia123@gmail.com>
2016-12-13 00:31:13 +0530

    Updated application by adding add item and delete item functionalities and adding new like and unline icons

--- GroceriesProject/js/script.js
+++ GroceriesProject/js/script.js
@@ -1,17 +1,33 @@
-var myApp = angular.module("myApp",[]);
+var myApp = angular.module("myApp", []);
 
-myApp.controller("myController",function ($scope){
-    var items = [{name:"CHEESE",likes:0,dislikes:0},
-                    {name:"EGG",likes:0,dislikes:0},
-                    {name:"ICE CREAM",likes:0,dislikes:0},
-                    {name:"POTATO",likes:0,dislikes:0},
-                    {name:"BREAD",likes:0,dislikes:0}];
+myApp.controller("myController", function ($scope) {
+    var items = [{ name: "C", likes: 0, dislikes: 0 },
+    { name: "JAVA", likes: 0, dislikes: 0 },
+    { name: "C#", likes: 0, dislikes: 0 },
+    { name: "PHP", likes: 0, dislikes: 0 },
+    { name: "HTML", likes: 0, dislikes: 0 }];
     $scope.items = items;
 
-    $scope.IncrementLIkes = function (item){
+    $scope.IncrementLIkes = function (item) {
         item.likes++;
     }
-    $scope.IncrementDisLikes = function (item){
+    $scope.IncrementDisLikes = function (item) {
         item.dislikes++;
+    }
+    $scope.InsertItem = function (item) {
+        item = item.toUpperCase();
+        var blnDontInsert = false;
+        angular.forEach(items, function (keyitem) {
+            if (keyitem.name == item)
+            { blnDontInsert = true; }
+            else { console.log(keyitem.name); }
+        });
+        if (!blnDontInsert) {
+            var newItem = { name: item, likes: 0, dislikes: 0 };
+            items.push(newItem);
+        }
+    }
+    $scope.RemoveItem = function (item) {
+        items.splice(items.indexOf(item), 1);
     }
 });
