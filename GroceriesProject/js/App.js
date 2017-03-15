47aa22f7f85ed918009dc32f0c77fde18fc1cef5
Ramakrishna <ramakrishna.pittu@pype.io>
2017-03-15 12:16:52 +0530

    check

--- GroceriesProject/js/App.js
+++ GroceriesProject/js/App.js
@@ -1,1 +1,12 @@
- var fileApp = angular.module('fileApp', []);
+///// <reference path="C:\Users\Dell\Documents\AngularJs\AngularJsProjects\GroceriesProject\lib\angular.min.js" />
+ var App = angular.module('app', [
+     'ui.router'
+ ]).config(['$urlRouterProvider','$stateProvider',function($urlRouterProvider,$stateProvider){
+     $urlRouterProvider.otherwise('/');
+
+     $stateProvider
+        .state('home',{
+            url : '/',
+            templateUrl : 'Bootstrap/Bootstrap.html'
+     });
+ }]);
